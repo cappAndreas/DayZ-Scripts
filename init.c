e58942c00130178d68312700649841b44068315d
cappAndreas <79117677+cappAndreas@users.noreply.github.com>
2024-03-12 08:49:09 +0100

    Create init.c

---
+++ init.c
@@ -0,0 +1,118 @@
+void main()
+{
+	//INIT ECONOMY--------------------------------------
+	Hive ce = CreateHive();
+	if ( ce )
+		ce.InitOffline();
+
+	//DATE RESET AFTER ECONOMY INIT-------------------------
+	int year, month, day, hour, minute;
+	int reset_month = 9, reset_day = 20;
+	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+
+	if ((month == reset_month) && (day < reset_day))
+	{
+		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+	}
+	else
+	{
+		if ((month == reset_month + 1) && (day > reset_day))
+		{
+			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+		}
+		else
+		{
+			if ((month < reset_month) || (month > reset_month + 1))
+			{
+				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+			}
+		}
+	}
+// Uncomment next two lines to extract mapgrouppos.xml and mapgroupcluster.xml data. Start server, let it run for a few minutes then stop it and comment the lines again.
+// GetCEApi().ExportProxyData( "7500 0 7500", 15000 );  // Center of map, radius of how far to go out and find buildings.
+// GetCEApi().ExportClusterData(); // Extracted files are in mission_folder/storage_380881/export
+}
+
+class CustomMission: MissionServer
+{
+	void SetRandomHealth(EntityAI itemEnt)
+	{
+		if ( itemEnt )
+		{
+			float rndHlt = Math.RandomFloat( 0.45, 0.65 );
+			itemEnt.SetHealth01( "", "", rndHlt );
+		}
+	}
+
+	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
+	{
+		Entity playerEnt;
+		playerEnt = GetGame().CreatePlayer( identity, characterName, pos, 0, "NONE" );
+		Class.CastTo( m_player, playerEnt );
+
+		GetGame().SelectPlayer( identity, m_player );
+
+		return m_player;
+	}
+
+	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+	{
+		EntityAI itemClothing;
+		EntityAI itemEnt;
+		ItemBase itemBs;
+		float rand;
+		
+		// Set custom fresh spawn loadout. Uncomment to use.
+		/* 
+		player.RemoveAllItems();
+		
+		ref TStringArray topsArray = {"Classname_of_top"};
+		ref TStringArray pantsArray = {"Classname_of_pants"};
+		ref TStringArray shoesArray = {"Classname_of_boots"};
+		ref TStringArray helmArray = {"Classname_of_headgear"};
+		ref TStringArray handsArray = {"Classname_of_gloves"};
+		
+		EntityAI item1 = player.GetInventory().CreateInInventory(topsArray.GetRandomElement());
+		EntityAI item2 = player.GetInventory().CreateInInventory(pantsArray.GetRandomElement());
+		EntityAI item3 = player.GetInventory().CreateInInventory(shoesArray.GetRandomElement());
+		EntityAI item4 = player.GetInventory().CreateInInventory(helmArray.GetRandomElement());
+		EntityAI item5 = player.GetInventory().CreateInInventory(handsArray.GetRandomElement());
+		 */
+
+		itemClothing = player.FindAttachmentBySlotName( "Body" );
+		if ( itemClothing )
+		{
+			SetRandomHealth( itemClothing );
+			
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
+			player.SetQuickBarEntityShortcut(itemEnt, 2);
+			
+			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
+			int rndIndex = Math.RandomInt( 0, 4 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+			player.SetQuickBarEntityShortcut(itemEnt, 1);
+
+			rand = Math.RandomFloatInclusive( 0.0, 1.0 );
+			if ( rand < 0.35 )
+				itemEnt = player.GetInventory().CreateInInventory( "Apple" );
+			else if ( rand > 0.65 )
+				itemEnt = player.GetInventory().CreateInInventory( "Pear" );
+			else
+				itemEnt = player.GetInventory().CreateInInventory( "Plum" );
+			player.SetQuickBarEntityShortcut(itemEnt, 3);
+			SetRandomHealth( itemEnt );
+		}
+		
+		itemClothing = player.FindAttachmentBySlotName( "Legs" );
+		if ( itemClothing )
+			SetRandomHealth( itemClothing );
+		
+		itemClothing = player.FindAttachmentBySlotName( "Feet" );
+	}
+};
+
+Mission CreateCustomMission(string path)
+{
+	return new CustomMission();
+}
